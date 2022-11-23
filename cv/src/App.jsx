474050467affab08da0bd1defa6bc70c1a9ffa56
ZHumay <114071477+ZHumay@users.noreply.github.com>
2022-11-23 08:09:11 -0800

    Add files via upload

---
+++ cv/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+ class App extends Component {
+  render() {
+    return (
+      <div class="container">
+        <img class="img" src='' alt='image'></img>
+        <p> About me: My name is Humay.I am 20. I live in Baku.I am reliable,responsible and hard-working person</p>
+      <ul> Skills
+      <li> HTML5</li>
+      <li> CSS3</li>
+      <li> Javascript</li>
+      <li>React</li>
+      </ul>
+      <ul>Contact:
+   <li> Mobile: +994 77 522 53 00 </li> 
+   <li>E-mail:zehmetihumay@gmail.com</li>
+        </ul>
+      <p> Education: Azerbaijan State Oil and Industry university(Process automation engineering)</p>
+      <p> Certifications: IKT LAB- IT essentials, Cisco Networking Academy- Essentials in Python</p>
+      </div>
+    )
+  }
+}
+export default App
